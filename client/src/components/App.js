590c52747ced7ffc611a3cbf41f063d6b4bbcdaf
volkovVA <volkov.slava@hotmail.com>
2022-04-04 15:05:16 +0300

    feat: add stream components & routes

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import StreamCreate from './streams/StreamCreate';
+import StreamDelete from './streams/StreamDelete';
+import StreamEdit from './streams/StreamEdit';
+import StreamList from './streams/StreamList';
+import StreamShow from './streams/StreamShow';
+import Header from './Header';
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div className="ui container">
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" exact element={<StreamList />} />
+          <Route path="/streams/new" element={<StreamCreate />} />
+          <Route path="/streams/edit" element={<StreamEdit />} />
+          <Route path="/streams/delete" element={<StreamDelete />} />
+          <Route path="/streams/show" element={<StreamShow />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
